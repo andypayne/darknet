7f69c93d7838f0de260897c9969d7e962bc0789a
andypayne <apayne@gmail.com>
2020-05-28 08:10:08 -0700

    Added a -datacfg option to allow passing a path to the data config file.

--- examples/darknet.c
+++ examples/darknet.c
@@ -433,8 +433,9 @@ int main(int argc, char **argv)
         float thresh = find_float_arg(argc, argv, "-thresh", .5);
         char *filename = (argc > 4) ? argv[4]: 0;
         char *outfile = find_char_arg(argc, argv, "-out", 0);
+        char *datacfg = find_char_arg(argc, argv, "-datacfg", "cfg/coco.data");
         int fullscreen = find_arg(argc, argv, "-fullscreen");
-        test_detector("cfg/coco.data", argv[2], argv[3], filename, thresh, .5, outfile, fullscreen);
+        test_detector(datacfg, argv[2], argv[3], filename, thresh, .5, outfile, fullscreen);
     } else if (0 == strcmp(argv[1], "cifar")){
         run_cifar(argc, argv);
     } else if (0 == strcmp(argv[1], "go")){
